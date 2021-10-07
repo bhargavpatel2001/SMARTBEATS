50c1567face39f4f35b7f3bcef692cdab74e4d64
Bhargav Patel <bhargavvalsad2001@gmail.com>
2021-10-07 15:22:55 -0400

    Cleaned up some space by deleting all the ViewModel.java and also started the color wheel in LightsFragment.java

--- SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/lights/LightsFragment.java
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/lights/LightsFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +15,27 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import ca.shalominc.it.smartbeats.R;
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class LightsFragment extends Fragment {
 
-    private LightsViewModel lightsViewModel;
+
+    private TextView gfgTextView;
+
+    // two buttons to open color picker dialog and one to
+    // set the color for GFG text
+    private Button mSetColorButton, mPickColorButton;
+
+    // view box to preview the selected color
+    private View mColorPreview;
+
+    // this is the default color of the preview box
+    private int mDefaultColor;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        lightsViewModel =
-                new ViewModelProvider(this).get(LightsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_lights, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        lightsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        return inflater.inflate(R.layout.fragment_lights, container, false);
     }
+
+    
 }
