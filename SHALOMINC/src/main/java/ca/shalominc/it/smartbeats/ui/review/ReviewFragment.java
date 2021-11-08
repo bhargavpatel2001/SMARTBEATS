99fbe8fcfa85eb27a289240a4d8ae5614c8605a8
Bhargav Patel <bhargavvalsad2001@gmail.com>
2021-11-07 22:54:01 -0500

    Used the functionality of SharedPref for the EditTexts in ReviewFragment.

--- SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/review/ReviewFragment.java
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/review/ReviewFragment.java
@@ -1,15 +1,20 @@
 package ca.shalominc.it.smartbeats.ui.review;
 
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import ca.shalominc.it.smartbeats.MainActivity;
 import ca.shalominc.it.smartbeats.R;
 
 
@@ -19,6 +24,12 @@ public class ReviewFragment extends Fragment
     String manufacturerName = Build.MANUFACTURER;
     TextView TESTING;
     String ModelNo;
+    Button shalomSubmit;
+    EditText shalomName, shalomPhone, shalomEmail, shalomComment;
+    int num;
+    String userValue, userValue2, userValue3;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -27,14 +38,56 @@ public class ReviewFragment extends Fragment
         return inflater.inflate(R.layout.fragment_review, container, false);
     }
 
-    public void onViewCreated(View view, Bundle savedInstanceState)
-    {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         TESTING = view.findViewById(R.id.TEST_WILL_BE_REMOVED_AFTER);
+        shalomSubmit = view.findViewById(R.id.submit_review_form_btn);
+        shalomPhone = view.findViewById(R.id.shalom_EditText_Phone);
+        shalomName = view.findViewById(R.id.shalom_EditText_PersonName);
+        shalomEmail = view.findViewById(R.id.shalom_EditText_EmailAddress);
+        shalomComment = view.findViewById(R.id.shalom_EditText_Comment);
 
+        //
         ModelNo = getModelNo();
-
         TESTING.setText(ModelNo);
+
+        //retriving
+        SharedPreferences shalomprefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        int num1 = shalomprefs.getInt("num",0);
+        shalomPhone.setText(""+ num1);
+
+        String value1 = shalomprefs.getString("userValue","1");
+        shalomName.setText(value1);
+
+        String value2 = shalomprefs.getString("userValue2","2");
+        shalomEmail.setText(value2);
+
+        String value3 = shalomprefs.getString("userValue3","3");
+        shalomComment.setText(value3);
+
+        // Submit buttons functionality
+        shalomSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                num = Integer.parseInt(shalomPhone.getText().toString());
+                userValue = shalomName.getText().toString();
+                userValue2 = shalomEmail.getText().toString();
+                userValue3 = shalomComment.getText().toString();
+
+                //Saving the data
+                SharedPreferences shalomprefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+                SharedPreferences.Editor change = shalomprefs.edit();
+
+                change.putInt("num",num);
+                change.putString("userValue",userValue);
+                change.putString("userValue2",userValue2);
+                change.putString("userValue3",userValue3);
+                change.apply();
+
+            }
+        });
     }
 
     public String getModelNo()
--- SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/settings/SettingsFragment.java
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/settings/SettingsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +25,6 @@ public class SettingsFragment extends Fragment {
 
     Switch portraitSwitch;
     int flag = 1;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
