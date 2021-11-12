5d93bf321f24a8a542523300523cd93bfba2451f
VidhiKanhye4573 <n01354573@humbermail.ca>
2021-11-11 22:47:38 -0500

    Added a java file called musicDuration to meet the 10 java files requirement as well as simplifying the content from the MusicFragment java file.

---
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/music/musicDuration.java
@@ -0,0 +1,28 @@
+package ca.shalominc.it.smartbeats.ui.music;
+
+import android.media.MediaPlayer;
+
+import java.util.concurrent.TimeUnit;
+
+public class musicDuration {
+
+    public static String getDurationText(MediaPlayer mediaP){
+        String d;
+
+        int duration = mediaP.getDuration();
+
+        d = convertFormat(duration);
+
+        return d;
+
+    }
+
+    public static String convertFormat(int duration) {
+
+        return String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(duration),
+                TimeUnit.MILLISECONDS.toSeconds(duration) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration)));
+    }
+
+}
