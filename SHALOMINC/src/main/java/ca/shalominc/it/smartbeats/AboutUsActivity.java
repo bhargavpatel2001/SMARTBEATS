5cbe1aa38b5a4ad7900a36c8b4e17560cdb35eb3
ripalPatel4619 <n01354619@humbermail.ca>
2021-12-01 13:49:13 -0500

    Connected AboutUs page to another activity by making new java file and xml file

---
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/AboutUsActivity.java
@@ -0,0 +1,14 @@
+package ca.shalominc.it.smartbeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
--- SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/settings/SettingsFragment.java
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/settings/SettingsFragment.java
@@ -1,28 +1,16 @@
 package ca.shalominc.it.smartbeats.ui.settings;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
-import com.google.android.material.snackbar.Snackbar;
-
+import ca.shalominc.it.smartbeats.AboutUsActivity;
 import ca.shalominc.it.smartbeats.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -54,6 +42,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
                         break;
                 }
+                return true;
+            }
+        });
+
+        Preference myPrefaboutUs = (Preference)findPreference("about_us");
+        myPrefaboutUs.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Intent intent;
+                intent = new Intent(getContext(), AboutUsActivity.class);
+                startActivity(intent);
+
                 return false;
             }
         });
