14cc9cfc4bf4cf4a56a988b9ea0b1a44cf494e11
ripalPatel4619 <n01354619@humbermail.ca>
2021-12-01 10:48:29 -0500

    Added the functionality for portrait switch in settings fragment using preferences and fixed the layout of review fragment

--- SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/settings/SettingsFragment.java
+++ SHALOMINC/src/main/java/ca/shalominc/it/smartbeats/ui/settings/SettingsFragment.java
@@ -18,6 +18,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -26,14 +27,52 @@ import ca.shalominc.it.smartbeats.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    int flag = 1;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.prefrences, rootKey);
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        Preference myPref = (Preference)findPreference("portrait_switch");
+        myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                switch (flag)
+                {
+                    case 1:
+                        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                        Toast.makeText(getContext(),R.string.lock_portrait,Toast.LENGTH_LONG).show();
+                        flag++;
+
+                        break;
+
+                    case 2:
+                        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                        Toast.makeText(getContext(),R.string.portrait_lock_disable,Toast.LENGTH_LONG).show();
+                        flag=1;
+
+                        break;
+                }
+                return false;
+            }
+        });
 
     }
-//
-//        public View onCreateView(@NonNull LayoutInflater inflater,
-//                                 ViewGroup container, Bundle savedInstanceState) {
+
+
+    @Override
+    public void onPrepareOptionsMenu(@NonNull Menu menu)
+    {
+        setHasOptionsMenu(true);
+        super.onPrepareOptionsMenu(menu);
+        menu.findItem(R.id.musicBtn).setVisible(false);
+        menu.findItem(R.id.lightsPwrBtn).setVisible(false);
+        menu.findItem(R.id.bluetoothBtn).setVisible(false);
+    }
+
+//    public View onCreateView(@NonNull LayoutInflater inflater,
+//                             ViewGroup container, Bundle savedInstanceState) {
 //
 //        return inflater.inflate(R.layout.fragment_settings, container, false);
 //    }
