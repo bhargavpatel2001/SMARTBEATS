e120153d07669e6921fc09721b76fdd695486029
Bhargav Patel <bhargavvalsad2001@gmail.com>
2021-12-04 19:47:46 -0500

    Completed the Settings fragment check in espresso mode

---
+++ SHALOMINC/src/androidTest/java/ca/shalominc/it/smartbeats/SettingsFragmentTest.java
@@ -0,0 +1,143 @@
+package ca.shalominc.it.smartbeats;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ca.shalominc.it.smartbeats.ui.settings.SettingsFragment;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SettingsFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void SettingsFragmentTest() {
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.navigation_settings), withContentDescription("Settings"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0),
+                                2),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction switch_ = onView(
+                allOf(withId(R.id.shalom_night_mode_switch), withText("Night Mode"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                4),
+                        isDisplayed()));
+        switch_.perform(click());
+
+        ViewInteraction switch_2 = onView(
+                allOf(withId(R.id.shalom_night_mode_switch), withText("Night Mode"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                4),
+                        isDisplayed()));
+        switch_2.perform(click());
+
+        ViewInteraction switch_3 = onView(
+                allOf(withId(R.id.shalom_portrait_switch), withText("Portrait Mode"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                5),
+                        isDisplayed()));
+        switch_3.perform(click());
+
+        ViewInteraction switch_4 = onView(
+                allOf(withId(R.id.shalom_portrait_switch), withText("Portrait Mode"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                5),
+                        isDisplayed()));
+        switch_4.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.about_us_btn), withText("About Us"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        pressBack();
+
+        ViewInteraction materialButton2 = onView(
+                allOf(withId(R.id.shalom_privacy_policy_btn), withText("Privacy Policy"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                1),
+                        isDisplayed()));
+        materialButton2.perform(click());
+
+        pressBack();
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.shalom_floatingbutton),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.nav_host_fragment),
+                                        0),
+                                6),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
